ea3ec717b51bd23a3f7f1c20e957f560d56b5942
haemiii <73984822+haemiii@users.noreply.github.com>
2022-11-16 12:33:18 +0900

    database 설정

--- src/server.js
+++ src/server.js
@@ -25,8 +25,8 @@ const options = {
   host: "127.0.0.1",
   user: "root",
   port: 3306,
-  password: "9401",
-  database: "nodejs",
+  password: "0000",
+  database: "newBlink",
 };
 
 const sessionStore = new MySQLStore(options);
