2fd8f8ec6b9cfe8484c8a88dcf9dd3299f1219ad
Marvic1130 <>
2022-12-03 20:06:14 +0900

    Server.js

---
+++ MachineLearning/Server.js
@@ -0,0 +1,7 @@
+const spawn = require("child_process").spawn;
+
+const result = spawn('python', ['./BlinkingRecognition.py'])
+
+result.stdout.on('data', function(data) {
+    console.log(data.toString())
+})
